9a5334eb65ac8e84f4c4b517a1e2c83400c47798
DBZLD <dbzld102@gmail.com>
2025-04-17 21:00:01 +0900

    4/17
    unity 한것

--- Assets/4.Scripts/Manager/UnitStatusManager.cs
+++ Assets/4.Scripts/Manager/UnitStatusManager.cs
@@ -9,7 +9,7 @@ public class UnitStatusManager : MonoBehaviour
         public int maxHp;               //최대 체력
         public int nowDamage;           //현재 공격력
         public int nowDefence;          //현재 방어력
-        public float moveSpeed;           //이동 속도
+        public float moveSpeed;         //이동 속도
         public int maxEnergy;           //최대 에너지
     }
     void Start()
